ee4eb34adc02249306f3951c67cc14557f5e6275
hnekhniadovich <nekhanya@gmail.com>
2019-04-09 13:28:56 -0400

    add technical skills

--- js/script.js
+++ js/script.js
@@ -6,10 +6,50 @@ $(document).ready(function() {
     });
 
     var typed = new Typed(".typed", {
-		strings: ["Software Engineer.", "Web Developer.", "Student."],
-		typeSpeed: 70,
+		strings: ["Full Stack Web Developer.", "Software Engineer.", "Web Designer.", "Student."],
+		typeSpeed: 60,
 		loop: true,
 		startDelay: 1000,
 		showCursor: false
+	});
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        items: 4,
+        responsive:{
+            0:{
+                items:1
+            },
+            480:{
+                items:2
+            },
+            768:{
+                items:3
+            },
+            938:{
+                items:4
+            }
+        }
+    });
+
+
+    var skillsTopOffset = $(".skillsSection").offset().top;
+
+	$(window).scroll(function() {
+
+		if(window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
+
+			$('.chart').easyPieChart({
+		        easing: 'easeInOut',
+		        barColor: '#fff',
+		        trackColor: false,
+		        scaleColor: false,
+		        lineWidth: 4,
+		        size: 152,
+		        onStep: function(from, to, percent) {
+		        	$(this.el).find('.percent').text(Math.round(percent));
+		        }
+		    });
+		}
 	});
 });
